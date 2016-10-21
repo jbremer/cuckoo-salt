f91e2bb022c2ea6c0211ec40a6c0dc3829253dfe
haam3r <haam3r@protonmail.com>
2016-10-21 11:34:29 +0300

    syntax fix

--- cuckoo/files/vmcloak_generate.sh
+++ cuckoo/files/vmcloak_generate.sh
@@ -3,5 +3,5 @@
 
 vmcloak init --win7x64 win7master --iso-mount {{ salt['pillar.get']('vmcloak:isomount')}}/ --ip {{ salt['pillar.get']('vmcloak:ipprefix')}}199 --gateway {{ salt['pillar.get']('vmcloak:ipprefix')}}1 --dns {{ salt['pillar.get']('vmcloak:dns')}} --ramsize {{ salt['pillar.get']('vmcloak:ramsize')}} --vramsize {{ salt['pillar.get']('vmcloak:vramsize')}}
 vmcloak install win7master adobepdf:9.4.0 wic pillow dotnet:4.6.1 java:7u71 flash:15.0.0.167 winrar cuteftp ie11 wallpaper extract extract.zip=/srv/iso/Archive.zip extract.dir=Desktop
-vmcloak install win7master office office.isopath=/srv/iso/Office_2010SP1_x64.ISO office.serialkey=CXQQF-QMQGT-WXWK6-T8XQM-FHDDR office.activate=1
+vmcloak install win7master office office.isopath=/srv/iso/Office_2010SP1_x64.ISO office.serialkey=XXXXX-XXXXX-XXXXX-XXXXX-XXXXX office.activate=1
 vmcloak snapshot --count {{ salt['pillar.get']('vmcloak:count') }} --cpus 4 --resolution 1280x1024 --ramsize 4096 win7master {{ salt['pillar.get']('vmcloak:basename') }} {{ salt['pillar.get']('vmcloak:ipprefix') }}{{ salt['pillar.get']('vmcloak:ipstart')|int + 1 }}
